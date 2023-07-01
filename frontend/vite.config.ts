b5e0bd23c1149c27bc41956ab4e632a5b2448267
Geza Szoradi <geza.szilard@vodafone.com>
2023-07-01 12:43:53 +0200

    Setup docker for FE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,30 @@
+import { resolve } from 'node:path';
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: resolve(__dirname, 'src'),
+        },
+      ],
+    },
+    plugins: [react(), viteCompression()],
+    server: {
+      port: 5000,
+      porxy: {
+        // TODO: Add proxy
+        '/api': {
+          target: 'http://jsonplaceholder.typicode.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
